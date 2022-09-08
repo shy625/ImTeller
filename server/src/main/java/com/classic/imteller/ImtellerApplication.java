ba8cebea112e48b126d896eb0bc1b0148e5c63e4
오석호 <dlscjs8646@naver.com>
2022-09-08 11:16:24 +0900

    :tada: 프로젝트 서버 환경설정
    잘된듯~~

---
+++ server/src/main/java/com/classic/imteller/ImtellerApplication.java
@@ -0,0 +1,13 @@
+package com.classic.imteller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImtellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImtellerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/classic/imteller/ImtellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classic.imteller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImtellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
