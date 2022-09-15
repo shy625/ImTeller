ae7ebabd5650943f3e0150fa0f6613fe305928f3
shy625 <thgusdud123@gmail.com>
2022-09-15 18:01:08 +0900

    :sparkles: Add BaseEntity

---
+++ server/src/main/java/com/classic/imteller/api/repository/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.classic.imteller.api.repository;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+    private Long id = null;
+
+    @CreatedDate
+    @Column(updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime updatedAt;
+
+}
