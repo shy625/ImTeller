3803b25f408bcccf7cc66b37afbeb259c95d233b
Ssoco <dlscjs8646@gmail.com>
2022-09-20 11:50:23 +0900

    :sparkles: 회원가입 컨트롤러 및 서비스 로직 추가

--- server/src/main/java/com/classic/imteller/api/controller/UserController.java
+++ server/src/main/java/com/classic/imteller/api/controller/UserController.java
@@ -21,28 +21,6 @@ import java.util.Date;
 public class UserController {
     private final UserService userService;
 
-    public String getRamdomPassword(int size) {
-        char[] charSet = new char[] {
-                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                '!', '@', '#', '$', '%', '^', '&' };
-
-        StringBuffer sb = new StringBuffer();
-        SecureRandom sr = new SecureRandom();
-        sr.setSeed(new Date().getTime());
-
-        int idx = 0;
-        int len = charSet.length;
-        for (int i=0; i<size; i++) {
-            // idx = (int) (len * Math.random());
-            idx = sr.nextInt(len);    // 강력한 난수를 발생시키기 위해 SecureRandom을 사용한다.
-            sb.append(charSet[idx]);
-        }
-
-        return sb.toString();
-    }
-
     @GetMapping("/email/{email}")
     @ApiOperation(value = "이메일 중복 체크", notes = "중복 이메일인지 체크")
     public ResponseEntity<String> hasEmail(@PathVariable String email){
@@ -58,12 +36,12 @@ public class UserController {
     public ResponseEntity<String> signUp(@RequestBody SignupReqDto signupReqDto) {
         // 서비스에 접근해서 DB에 이메일과 비밀번호, 닉네임을 등록
         // user_id, 프로필, 경험치, 승, 패, 생성일시, 수정일시는 기본으로 들어갈 수 있도록 처리한다.
-        userService.
+        Boolean result = userService.signUp(signupReqDto);
         // 잘 등록됐으면 그대로 리턴
-        return new ResponseEntity<String>("가입 성공", HttpStatus.ACCEPTED);
+        if (result) return new ResponseEntity<String>("가입 성공", HttpStatus.ACCEPTED);
 
         // 등록 안됐으면 forbidden 리턴
-        return new ResponseEntity<String>("가입 성공", HttpStatus.FORBIDDEN);
+        return new ResponseEntity<String>("가입 실패", HttpStatus.FORBIDDEN);
     }
 
 }
--- server/src/main/java/com/classic/imteller/api/service/UserService.java
+++ server/src/main/java/com/classic/imteller/api/service/UserService.java
@@ -1,15 +1,58 @@
 package com.classic.imteller.api.service;
 
+import com.classic.imteller.api.dto.user.SignupReqDto;
+import com.classic.imteller.api.repository.User;
 import com.classic.imteller.api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.security.SecureRandom;
+import java.util.Date;
 
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    // 이메일 체크
     public Boolean hasEmail(String email) {
         int i = userRepository.countByEmail(email);
         return i > 0;
     }
 
-    public boolean
+    public String getRandomPassword(int size) {
+        char[] charSet = new char[] {
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                '!', '@', '#', '$', '%', '^', '&' };
+
+        StringBuffer sb = new StringBuffer();
+        SecureRandom sr = new SecureRandom();
+        sr.setSeed(new Date().getTime());
+
+        int idx = 0;
+        int len = charSet.length;
+        for (int i=0; i<size; i++) {
+            // idx = (int) (len * Math.random());
+            idx = sr.nextInt(len);    // 강력한 난수를 발생시키기 위해 SecureRandom을 사용한다.
+            sb.append(charSet[idx]);
+        }
+
+        return sb.toString();
+    }
+
+    // 회원가입
+    public Boolean signUp(SignupReqDto signupReqDto) {
+        User newUser = User.builder()
+                .email(signupReqDto.getEmail())
+                .password(getRandomPassword(10))
+                .nickname(signupReqDto.getNickname())
+                .profile("DEFAULT")
+                .exp(0)
+                .win(0)
+                .lose(0)
+                .build();
+        if (userRepository.save(newUser) != null) return true;
+        else return false;
+    }
 }
