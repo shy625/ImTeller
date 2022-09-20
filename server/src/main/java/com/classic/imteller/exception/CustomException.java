9dff2a5b4a777cc00a4211721efe97abd56b830a
Ssoco <dlscjs8646@gmail.com>
2022-09-20 22:48:27 +0900

    :sparkles: 에러코드 관리 부분 추가

---
+++ server/src/main/java/com/classic/imteller/exception/CustomException.java
@@ -0,0 +1,10 @@
+package com.classic.imteller.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException{
+    private final ErrorCode errorCode;
+}
