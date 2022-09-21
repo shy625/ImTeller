9c0e591efb7247ccf6ae19ec1ae739dd41b179d6
김보경 <rawoon@kakao.com>
2022-09-21 15:17:33 +0900

    :sparkles: gameroom created

--- client/src/pages/GameRoom/GameRoom.tsx
+++ client/src/pages/GameRoom/GameRoom.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
+import RoomInfo from '@components/roomInfo'
+// 여기서 이제 socket 연결이 시작되는거지
 export default function GameRoom() {
   const roomId: any = useParams().gameId
 
