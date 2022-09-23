a45154cc8376a824bcb8ab895c2f058bbdc256cb
shy625 <thgusdud123@gmail.com>
2022-09-23 15:01:52 +0900

    :sparkles: Add common Response Dto

---
+++ server/src/main/java/com/classic/imteller/api/dto/ResponseDto.java
@@ -0,0 +1,12 @@
+package com.classic.imteller.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDto {
+
+    private Object response;
+
+}
