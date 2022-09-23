cf296428a53ace242a0482b1412056dc4f7334d0
shy625 <thgusdud123@gmail.com>
2022-09-23 15:01:09 +0900

    :sparkles: Update Deal entity

--- server/src/main/java/com/classic/imteller/api/repository/Deal.java
+++ server/src/main/java/com/classic/imteller/api/repository/Deal.java
@@ -1,13 +1,42 @@
 package com.classic.imteller.api.repository;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
+@NoArgsConstructor
 @Entity
 public class Deal extends BaseEntity {
 
+    @OneToOne
+    private Card card;
 
+    @Column(nullable = false)
+    private Integer lowPrice;
 
+    @Column(nullable = false)
+    private Integer instantPrice;
+
+    @Column
+    private String tag;
+
+    @Column(nullable = false, columnDefinition = "DATETIME")
+    private LocalDateTime finishedAt;
+
+    @OneToMany(mappedBy = "deal")
+    private List<Bid> bidList;
+
+    @Builder
+    public Deal(Card card, Integer lowPrice, Integer instantPrice, String tag, LocalDateTime finishedAt) {
+        this.card = card;
+        this.lowPrice = lowPrice;
+        this.instantPrice = instantPrice;
+        this.tag = tag;
+        this.finishedAt = finishedAt;
+    }
 }
