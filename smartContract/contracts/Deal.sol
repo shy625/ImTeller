bc85d245119ae1e12c00511ad301696dd30ad7dc
minji <whiterisi@naver.com>
2022-09-27 15:55:18 +0900

    :sparkles: edit deal

--- smartContract/contracts/Deal.sol
+++ smartContract/contracts/Deal.sol
@@ -11,12 +11,10 @@ contract Deal {
     // 생성자에 의해 정해지는 값
     address public seller;
     address public buyer;
-    uint256 public dealStartTime;
-    uint256 public dealEndTime;
+    uint256 public dealTime;
     uint256 public minPrice;
     uint256 public purchasePrice;
     uint256 public cardId;
-    address public currencyAddress;
     address public nftAddress;
     bool public ended;
 
@@ -24,9 +22,7 @@ contract Deal {
     address public highestBidder;
     uint256 public highestBid;
 
-    IERC20 public erc20Contract;
-    IERC721 public erc721Contract;
-    address private TockenAddress;
+    IERC721 public cardContract;
     address private NFTAddress;
 
     event NewBid(address dealContractAddress, uint cardId, address bidder, uint256 amount);
@@ -38,9 +34,7 @@ contract Deal {
         uint256 _cardId,
         uint256 _minPrice,
         uint256 _purchasePrice,
-        uint256 startTime,
-        uint256 endTime,
-        address _currencyAddress,
+        uint256 _dealTime,
         address _nftAddress
     ) {
         require(_minPrice > 0);
@@ -48,39 +42,32 @@ contract Deal {
         minPrice = _minPrice;
         purchasePrice = _purchasePrice;
         seller = _seller;
-        dealStartTime = startTime;
-        dealEndTime = endTime;
-        currencyAddress = _currencyAddress;
+        dealtime = now + _dealTime;
         nftAddress = _nftAddress;
         ended = false;
         highestBid=0;
         highestBidder=address(0);
-        erc20Contract = IERC20(_currencyAddress);
-        erc721Contract = IERC721(_nftAddress);
-        TockenAddress = _currencyAddress;
+        cardContract = IERC721(_nftAddress);
         NFTAddress = _nftAddress;
     }
 
 
-    function bid(uint256 amount) public  duringDeal returns (bool){
+    function bid(uint256 amount) public payable duringDeal returns (bool){
         address bidder = msg.sender;
 
-        require(erc20Contract.balanceOf(bidder) >= amount, "Bidder have not enough Token");
-        require(erc20Contract.approve(bidder, amount), "Not approved for ERC20");
         require(amount >= minPrice, "Your bid is lower than MinPrice");
         require(amount > highestBid, "Your bid is lower than HighestBid");
         require(amount < purchasePrice, "Your bid have to lower than Purchase Price");
 
         // 새로운 Highet bidder 등장에 따른 기존의 Highet bidder 환불
         if (highestBidder != address(0)) {
-            //erc20Contract.approve(address(this), highestBid);
-            //erc20Contract.transferFrom(address(this), highestBidder, highestBid);
-            Token(TockenAddress).forceToTransfer(address(this), highestBidder, highestBid);
+
+            //
+
         }
 
         //bidder contract 로 송금
-        //erc20Contract.transferFrom(bidder, address(this), amount);
-        Token(TockenAddress).forceToTransfer(bidder, address(this), amount);
+        //
 
         //최고입찰가, 입찰가 정보 업데이트
         highestBid = amount;
@@ -102,19 +89,15 @@ contract Deal {
 
         //1. 최고입찰자가 존재하는 경우 ERC20 토큰 환불진행
         if(highestBidder!=address(0)){
-            //erc20Contract.approve(address(this), highestBid);
-            //erc20Contract.transferFrom(address(this), highestBidder, highestBid);
-            Token(TockenAddress).forceToTransfer(address(this), highestBidder, highestBid);
+
+            //
+
         }
         //2. 구매자의 ERC20 토큰을 판매자에게 송금, 즉시구매가
-        //erc20Contract.transferFrom(buyer, seller, purchasePrice);
-        Token(TockenAddress).forceToTransfer(buyer, seller, purchasePrice);
+        //
 
         //3. 판매자의 NFT를 구매자에게 소유권 이전
-        // erc721Contract.approve(buyer, cardId);
-        // erc721Contract.transferFrom(address(this), buyer, cardId);
-        //소유권, 권한 수정하면서 이전시켜야되는데 그거 잘 안되서 일단 강제 이전시킴
-        ClassicNFT(NFTAddress).forceToTransfer(seller, buyer, cardId);
+        ClassicNFT(NFTAddress).transferFrom(address(this), buyer, cardId);
 
         //거래상태 종료로 변경
         _end();
@@ -125,28 +108,29 @@ contract Deal {
 
 
     //경매시간종료 후 최고 입찰자가 거래 실행
-    function confirmItem() public onlyAfterEnd {
+    function confirmItem() public payable onlyAfterEnd {
         buyer=msg.sender;
         require(buyer == highestBidder || buyer == seller, "caller is not highestBidder or seller");
-        Token(TockenAddress).forceToTransfer(address(this), highestBidder, highestBid);
-        ClassicNFT(NFTAddress).forceToTransfer(seller, buyer, cardId);
+
+        //
+
+        ClassicNFT(NFTAddress).transferFrom(address(this), buyer, cardId);
         emit DealEnded(address(this), cardId, buyer, highestBid);
         _end();
     }
 
     //판매자가 판매를 취소
-    function cancelDeal() public onlySeller{
+    function cancelDeal() public payable onlySeller{
         //경매참여자 있으면 최고입찰자에게 해당 금액 환불
         if(highestBidder!=address(0)){
-            erc20Contract.approve(address(this), highestBid);
-            erc20Contract.transferFrom(address(this), highestBidder, highestBid);
+
+            //
+
         }
         //카드 소유권을 다시 판매자에게 반환
-        //trial_1: 소유권 이전 권한 부여 : fail 이거자체가 새로운 에러 발생시키는중...
-        //erc721Contract.setApprovalForAll(address(this), true);
-        //trial_2: safetransferFrom -> transferFrom 으로 변경
-        //erc721Contract.transferFrom(address(this), seller, cardId);
+        ClassicNFT(NFTAddress).transferFrom(address(this), seller, cardId);
         //판매 상태를 완료로 전환
+
         _end();
         emit DealCanceled(address(this), cardId, seller);
     }
@@ -164,26 +148,18 @@ contract Deal {
         ended = true;
     }
 
-    function _getCurrencyAmount() private view returns (uint256) {
-        return erc20Contract.balanceOf(msg.sender);
-    }
-
     modifier onlySeller() {
         require(msg.sender == seller, "Deal: You are not seller.");
         _;
     }
 
     modifier duringDeal() {
-        require(block.timestamp >= dealStartTime,"Deal: This sale is not started.");
         require( !ended, "Deal: This sale is already Ended.");
         _;
     }
 
     modifier onlyAfterEnd() {
-        require(
-            block.timestamp > dealEndTime,
-            "Sale: This sale is not started."
-        );
+        require(ended, "Sale: This sale is not ended.");
         _;
     }
 }
--- smartContract/contracts/DealController.sol
+++ smartContract/contracts/DealController.sol
@@ -4,26 +4,18 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "./Token.sol";
 import "./ClassicNFT.sol";
 import "./Deal.sol";
 
 contract DealController is Ownable {
 
-    address[]  public deals;
+    constructor(address _nftAddress) {cardContract = IERC721(_nftAddress);}
 
-    IERC20 public erc20Contract;
-    IERC721 public erc721Contract;
+    address[]  public deals;
+    IERC721 public cardContract;
 
     event NewDeal(address seller, uint256 cardId, uint256 price);
 
-    constructor(
-        address _currencyAddress,
-        address _nftAddress
-    ) {
-        erc20Contract = IERC20(_currencyAddress);
-        erc721Contract = IERC721(_nftAddress);
-    }
     /*
     신규 거래 생성
     @params: 판매할 NFT(cardId), 입찰최저가격(minPrice), 즉시구매가(purchasePrice), 거래시작시간, 거래종료시간, ERC20 CA, ERC721 CA
@@ -35,7 +27,6 @@ contract DealController is Ownable {
         uint256 purchasePrice,
         uint256 startTime,
         uint256 endTime,
-        address currencyAddress,
         address nftAddress
     ) public returns (address) {
         address seller = msg.sender;
@@ -47,7 +38,7 @@ contract DealController is Ownable {
         Deal deal = new Deal(seller, cardId, minPrice, purchasePrice, startTime, endTime, currencyAddress, nftAddress);
 
         //3. 카드 소유권을 거래인스턴스에게 이전
-        //erc721Contract.transferFrom(seller, address(deal), cardId);
+        cardContract.transferFrom(seller, address(deal), cardId);
 
         //거래 CA 리스트에 신규 생성된 거래 push
         deals.push(address(deal));
@@ -62,15 +53,6 @@ contract DealController is Ownable {
     //생성된 모든 거래 CA 반환
     function allDeals() public view returns (address[] memory) {
         return deals;
-    }
-
-    /*
-    NFT 카드의 현재 보유자 반환
-    @params: NFT(cardId)
-    @return: 해당 NFT의 현재 보유자 주소
-    */
-    function findOwner(uint256 cardId) public view returns (address){
-        return erc721Contract.ownerOf(cardId);
     }
 
 }
