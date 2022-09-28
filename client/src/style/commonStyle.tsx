4d9bff9bdbaf775f016e501980b1a4502f304f0e
김보경 <rawoon@kakao.com>
2022-09-28 14:50:54 +0900

    :memo: add info tp common style

--- client/src/style/commonStyle.tsx
+++ client/src/style/commonStyle.tsx
@@ -1,5 +1,6 @@
 import { css } from '@emotion/react'
 
+// 와이어프레임에 나온 기본 버튼
 const normalBtn = css({
   outline: 'none',
   cursor: 'pointer',
@@ -11,6 +12,10 @@ const normalBtn = css({
   borderRadius: 12,
   fontSize: 13,
   width: '8em',
+  fontFamily: 'GongGothicMedium',
+})
+const textBtn = css({
+  cursor: 'pointer',
 })
 const input = css({
   padding: '10px 10px',
@@ -19,7 +24,7 @@ const input = css({
   border: 'none',
   borderRadius: '12px',
   width: '95%',
-  fontFamily: 'normalFont',
+  fontFamily: 'GongGothicMedium',
 })
 const textarea = css({
   padding: '10px 10px',
@@ -33,9 +38,6 @@ const textarea = css({
 const imgIcon = css({
   width: 20,
   height: 20,
-})
-const textBtn = css({
-  cursor: 'pointer',
 })
 const fullDisplay = css({
   height: 'auto',
