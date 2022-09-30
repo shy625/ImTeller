87bae94bd5cd7ec1a4bc9bbf1b7253382c411d7b
김보경 <rawoon@kakao.com>
2022-09-30 11:00:32 +0900

    :wrench: paging ongoing

--- client/src/pages/Mypage/mypageTabNav.tsx
+++ client/src/pages/Mypage/mypageTabNav.tsx
@@ -27,7 +27,7 @@ export default function MypageTabNav(props: any) {
             </div>
             <div
               onClick={() => {
-                navigate('/paint', { state: { isNew: true, setTabNo: setTabNo } })
+                navigate('/paint', { state: { isNew: true } })
               }}
             >
               그림그리기
