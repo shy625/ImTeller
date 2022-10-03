dd7dd32b329b9f04e56cfe42e375a1a68841ef19
minji <whiterisi@naver.com>
2022-10-04 02:28:32 +0900

    :lipstick: Css in RankPage

---
+++ client/src/pages/Rank/rankList.tsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+
+import React, { useState } from 'react'
+
+export default function rankList(props: any) {
+	const { tabNo } = props
+
+	return <div css={box}></div>
+}
+const box = css`
+	border: 3px solid rgb(55, 20, 159);
+`
