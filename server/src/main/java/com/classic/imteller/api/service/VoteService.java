57d92e3014102072d8dc4621185dfe4d0ad53fd8
minji <whiterisi@naver.com>
2022-10-04 01:47:24 +0900

    :bug: Add @transactional

--- server/src/main/java/com/classic/imteller/api/service/VoteService.java
+++ server/src/main/java/com/classic/imteller/api/service/VoteService.java
@@ -7,6 +7,7 @@ import com.classic.imteller.exception.CustomException;
 import com.classic.imteller.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,7 +46,7 @@ public class VoteService {
         vote.updateIsVoting(2);
         voteRepository.save(vote);
     }
-
+    @Transactional
     public String votePaint(String userNickname, long artId){
         String result;
         User user = userRepository.findByNickname(userNickname).orElseThrow(() -> new CustomException(ErrorCode.POSTS_NOT_FOUND));
