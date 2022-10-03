7e74855f2cf3dbc587f7e9f429739af62af24bf1
Ssoco <dlscjs8646@gmail.com>
2022-10-03 19:27:28 +0900

    :sparkles: isVoting will be 2 when paint is elected

--- server/src/main/java/com/classic/imteller/api/service/VoteService.java
+++ server/src/main/java/com/classic/imteller/api/service/VoteService.java
@@ -2,7 +2,10 @@ package com.classic.imteller.api.service;
 
 import com.classic.imteller.api.repository.Art;
 import com.classic.imteller.api.repository.ArtRepository;
+import com.classic.imteller.api.repository.Vote;
 import com.classic.imteller.api.repository.VoteRepository;
+import com.classic.imteller.exception.CustomException;
+import com.classic.imteller.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -22,5 +25,13 @@ public class VoteService {
     public List<Art> getVotePaints() {
         List<Art> res = artRepository.findAllByTokenIdAndIsVote();
         return res;
+    }
+
+    public void electedPaint(Long artId) {
+        // artId를 바탕으로, 현재 진행중인 vote에서 해당하는 artId를 찾아서
+        // vote테이블의 isVoting을 2로 바꾼다
+        Vote vote = voteRepository.findByArtIdAndIsVoting(artId, 1).orElseThrow(() -> new CustomException(ErrorCode.POSTS_NOT_FOUND));
+        vote.updateIsVoting(2);
+        voteRepository.save(vote);
     }
 }
