a8398eaeeb72e89dbf3a59ba08e208ff7a5cdc17
김보경 <rawoon@kakao.com>
2022-10-05 05:33:46 +0900

    :lipstick: pages

--- client/src/pages/DealList/DealList.tsx
+++ client/src/pages/DealList/DealList.tsx
@@ -1,7 +1,5 @@
-import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-
 /** @jsxImportSource @emotion/react */
+import { useSelector } from 'react-redux'
 import { css } from '@emotion/react'
 import Search from 'pages/DealList/search'
 import Layout from 'layout/layout'
@@ -9,15 +7,8 @@ import Deal from 'components/deal'
 import { fullDisplay } from 'style/commonStyle'
 
 export default function DealList() {
-	const navigate = useNavigate()
-
 	const dealList = useSelector((state: any) => state.dealList)
 	const currentUser = useSelector((state: any) => state.currentUser)
-
-	const onMove = () => {
-		if (!currentUser.nickname) return navigate('/login')
-		return navigate('/deal/register')
-	}
 
 	return (
 		<Layout>
@@ -25,9 +16,9 @@ export default function DealList() {
 				<div css={centerCSS}>
 					<div css={listWrapper}>
 						<div css={nav}>
-							<button css={button} onClick={onMove}></button>
 							<Search />
 						</div>
+
 						<div css={centerCSS}>
 							{dealList.map((deal: any) => (
 								<Deal deal={deal} key={deal.dealId} />
@@ -51,42 +42,12 @@ const nav = css`
 	display: flex;
 	flex-direction: row;
 	justify-content: space-between;
-	margin: 30px;
+	margin: 10px;
+	width: 90%;
 `
 const listWrapper = css`
-	width: 80%;
+	width: 70%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 `
-
-const button = css`
-	font-family: 'GmarketSansMedium';
-	outline: none;
-	height: 40px;
-	text-align: center;
-	width: 130px;
-	border-radius: 40px;
-	border: 1px solid white;
-	color: #f9f6f6;
-	background: rgb(74, 59, 117);
-	letter-spacing: 1px;
-	text-shadow: 0;
-	font: {
-		size: 12px;
-		weight: bold;
-	}
-	cursor: pointer;
-	transition: all 0.25s ease;
-	&:hover {
-		font-family: 'GongGothicMedium';
-		color: black;
-		background: rgb(255, 255, 255, 0.6);
-		border: 2px solid black;
-	}
-	&:active {
-		//letter-spacing: 2px;
-		letter-spacing: 2px;
-	}
-	&:after {
-		content: '판매등록';
-	}
-`
-const search = css``
