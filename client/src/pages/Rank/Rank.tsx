f7db5c5c871b3ec4d1334b48929d40730acaf711
김보경 <rawoon@kakao.com>
2022-10-07 05:35:40 +0900

    :bug: last month's most popular paint data changed

--- client/src/pages/Rank/Rank.tsx
+++ client/src/pages/Rank/Rank.tsx
@@ -78,7 +78,7 @@ export default function Rank() {
 		0: <RankListDeal rankList={rankList} />,
 		1: <RankListDealWin rankList={rankList} />,
 		2: <RankListDealLevel rankList={rankList} />,
-		3: <RankOne rankList={tempDummy} />,
+		3: <RankOne rankList={rankList} />,
 	}
 	return (
 		<Layout>
